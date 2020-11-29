f682413d9b145c36c4f39eebad31335fe0919c96
fxsth <fxsth@pm.me>
2020-11-29 12:46:59 +0100

    optional argument output dir

--- audiobook-organizer.py
+++ audiobook-organizer.py
@@ -8,6 +8,7 @@ if(len(sys.argv)<2):
     print("audiobook-organizer needs the directory of the audiobook as first argument")
     print("Optional second argument: \"Author - Title\"")
     print("Without second argument search term is set to folder name")
+    print("Optional third argument is Output directory such that OutputDirectory/author/audiobook")
     print("For example: audiobook-organizer.py \"/path/to/audiobook files\" \"J.K. Rowling - Harry Potter und der Stein der Weisen\"")
 
     exit()
@@ -23,6 +24,8 @@ print("author: " + author, " title: " + title)
 audiobookmeta = Audiobookmeta(searchterm)
 metadataFound = audiobookmeta.tryRetrieveFromITunes()
 outputDir = dir+"/"+author+"/"+title
+if(len(sys.argv)==4):
+    outputDir = sys.argv[3]+"/"+author+"/"+title
 outputDir = outputDir.replace(" / ", "/")
 # Create Output Directory
 splitter.create_dir(outputDir)
